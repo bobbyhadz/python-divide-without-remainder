70259e75ced637b8ff96152c5c6c3561477ec732
bobbyhadz <opmftw@gmail.com>
2024-04-09 07:56:04 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# Divide without a remainder in Python
+
+result_1 = 30 // 6
+print(result_1)  # 👉️ 5
+print(type(result_1))  # 👉️ <class 'int'>
+
+result_2 = 30 / 6
+print(result_2)  # 👉️ 5.0
+print(type(result_2))  # 👉️ <class 'float'>
